419438b06cdb52f91af7bc8c95b9f65ad05e3d3e
sullaspqr <40822691+sullaspqr@users.noreply.github.com>
2020-01-22 13:11:58 +0100

    Add files via upload
    Egyszerű fájlkezelés - beolvasás CSV-ből és file.ReadAllLines()-al soronként olvasás!

---
+++ egyszerufajlkez/egyszerufajlkez/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace egyszerufajlkez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"U:\szamok.csv";
+            string[] a = File.ReadAllLines(path);
+            for (int i = 0; i < a.Length; i++) {
+                Console.WriteLine(a[i]);
+            }
+                Console.ReadKey();
+            }
+        }
+    }
